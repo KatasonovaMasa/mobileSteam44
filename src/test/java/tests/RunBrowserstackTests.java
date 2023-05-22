0ec4a4c04f3f6bad7a0b3ae7f16d1086cdf75d34
Катасонова Мария <katasonova_masa@mail.ru>
2023-05-22 08:47:49 +0300

    fк

--- src/test/java/tests/RunBrowserstackTests.java
+++ src/test/java/tests/RunBrowserstackTests.java
@@ -25,7 +25,7 @@ import static io.appium.java_client.touch.offset.PointOption.point;
 import static io.qameta.allure.Allure.step;
 import static java.time.temporal.ChronoUnit.MILLIS;
 
-public class RunBrowserstackTests extends TestBaseBrowserstack {
+public class RunBrowserstackTests extends TestBaseTwoDrivers {
     static AuthorizationConfig config = ConfigFactory.create(AuthorizationConfig.class, System.getProperties());
     @Test
     @Tag("android")
